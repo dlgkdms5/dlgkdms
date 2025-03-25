c71cc57ce765acf7db3905fc6449dc6e048c38ab
dlgkdms5 <haeun5413@gmail.com>
2025-03-25 11:33:43 +0900

    Add files via upload

---
+++ 4주차 실습.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int score[10];
+	int sum = 0;
+	double average;
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d번째 학생 성적을 입력하시오 : ", i+1);
+		scanf("%d", &score[i]);
+		sum += score[i];
+	}
+
+	average = sum / 10.0;
+	printf("10명의 학생 총 점수 : %d\n", sum);
+	printf("10명의 학생 평균 점수 : %.2f\n", average);
+
+	return 0;
+
+}
